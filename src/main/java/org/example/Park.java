1d10749ba0fb638ec4f434db76018fb417e44667
galinaB-95 <galinabilokur@gmail.com>
2025-02-28 02:06:06 +0300

    lesson5

---
+++ src/main/java/org/example/Park.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Park {
+    private String название;
+    Atraktion atraktion;
+    Park.Atraktion atraktion = new Park().new Atraktion();
+    public class Atraktion {
+
+        private String времяРаботы;
+        private String стоимость;
+
+        public Park ()
+
+    }
+
+    public void main(String[] args) {
+        Park park1 = new Park();
+    }}
