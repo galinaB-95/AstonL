083724db8404f387b4a8d75d7ac48c0e2bf926b7
galinaB-95 <galinabilokur@gmail.com>
2025-03-09 17:52:58 +0300

    lesson

--- src/main/java/org/example/Array.java
+++ src/main/java/org/example/Array.java
@@ -1,16 +1,17 @@
 package org.example;
 
 public class Array {
-    public  int fourArray (String arr [] ){
-        int a = 1;
-        int [][] b = new  int[4][4];
+    public static void fourArray (String [] [] arr) {
+
+       String [][] n = new  String[4][4];
         for (int i=0; i<4; i++){
             for (int j = 0; j<4;  j++){
-                b [i][j] = a;
-                System.out.println(b[i][j] + "");
-                a++;
+
+                System.out.print(arr[i][j] + "");
+
             }
+            System.out.println();
         }
-        return a;
+
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,41 @@
 package org.example;
 
 
-public class Main {
+import java.util.Arrays;
 
+public class Main {
     public static void main(String[] args) {
-        Array[] fourArray = new Array[4];
-     Array.fourArray(
+        String[][] arr = new String[][]{{"1", "1", "5", "2"}, {"1", "2", "3", "4"}, {"1", "hi", "2", "1"}, {"1", "1", "3", "1"}};
+        try {
+            try {
+                int result = fourArray(arr);
+                System.out.println(result);
+            } catch (MyArraysSizeException e) {
+                System.out.println("Размер массива привышен");
+            }
+        } catch (MyArrayDataException e) {
+            System.out.println("Ошибка в ячейке " + e.i + "x" + e.j);
+        }
+    }
+
+    public static int fourArray(String[][] arr) throws MyArraysSizeException, MyArrayDataException {
+
+        int a = 1;
+        if (arr.length != 4) {
+            throw new MyArraysSizeException();
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].length != 4) {
+                throw new MyArraysSizeException();
+            }
+            for (int j = 0; j < arr[i].length; j++) {
+                try {
+                    a = a + Integer.parseInt(arr[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException(i, j);
+                }
+            }
+        }
+        return a;
     }
 }
