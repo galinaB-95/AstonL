986552dcb602260eaf504f9e31ceaa2ebe9b7ac8
galinaB-95 <galinabilokur@gmail.com>
2025-03-15 23:42:39 +0300

    lesson

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -39,6 +39,8 @@ public class Student {
         }
     }
 
-    public static void transferCourse(ArrayList<Student> students)
+    public static void transferCourse(ArrayList<Student> students){
+
+    }
 }
 
