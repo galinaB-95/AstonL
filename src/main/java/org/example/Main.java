a06722207de4fc6376147b87d12fbb2b7d5bd4fb
galinaB-95 <galinabilokur@gmail.com>
2025-03-15 23:27:03 +0300

    lesson

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,16 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(new Student("Ben", 120362, 2, new int [] {2,3,2,3}));
+        students.add (new Student("Adam", 463843, 3, new int [] {3, 4, 3, 3}));
+        students.add (new Student("Pam", 369065, 1, new int[] {2,3, 4, 3}));
+        System.out.println(students.size());
+        Student.deleteStudent(students);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
+
 }
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Student {
+    private String name;
+    private int group;
+    private int cours;
+    private int [] grades;
+    public Student (String name, int group, int cours, int[] grades) {
+        this.name = name;
+        this.group = group;
+        this.cours = cours;
+        this.grades = grades;
+    }
+    public int[] getGrades(){
+        return grades;
+    }
+  private static double averageGrade(int [] grades){
+
+      double a = ((Arrays.stream(grades).sum())/(double)grades.length);
+
+      return a;
+  }
+public static void deleteStudent(ArrayList<Student>students){
+        int lengthOfStudents = students.size();
+        int i = 0;
+        while (i<lengthOfStudents){
+            if (averageGrade(students.get(i).grades)<3){
+                students.remove(i);
+                lengthOfStudents--;
+                students.get(i).cours++;
+            }
+            else i++;
+        }
+}
+public static void
+}
+
