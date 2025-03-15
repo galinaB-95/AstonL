28e1128e97ac15f6bc8a47580367239b0dc94d9b
galinaB-95 <galinabilokur@gmail.com>
2025-03-15 23:27:45 +0300

    lesson

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Student> students = new ArrayList<>();
-        students.add(new Student("Ben", 120362, 2, new int [] {2,3,2,3}));
-        students.add (new Student("Adam", 463843, 3, new int [] {3, 4, 3, 3}));
-        students.add (new Student("Pam", 369065, 1, new int[] {2,3, 4, 3}));
+        students.add(new Student("Ben", 120362, 2, new int[]{2, 3, 2, 3}));
+        students.add(new Student("Adam", 463843, 3, new int[]{3, 4, 3, 3}));
+        students.add(new Student("Pam", 369065, 1, new int[]{2, 3, 4, 3}));
         System.out.println(students.size());
         Student.deleteStudent(students);
 
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -7,34 +7,38 @@ public class Student {
     private String name;
     private int group;
     private int cours;
-    private int [] grades;
-    public Student (String name, int group, int cours, int[] grades) {
+    private int[] grades;
+
+    public Student(String name, int group, int cours, int[] grades) {
         this.name = name;
         this.group = group;
         this.cours = cours;
         this.grades = grades;
     }
-    public int[] getGrades(){
+
+    public int[] getGrades() {
         return grades;
     }
-  private static double averageGrade(int [] grades){
 
-      double a = ((Arrays.stream(grades).sum())/(double)grades.length);
+    private static double averageGrade(int[] grades) {
 
-      return a;
-  }
-public static void deleteStudent(ArrayList<Student>students){
+        double a = ((Arrays.stream(grades).sum()) / (double) grades.length);
+
+        return a;
+    }
+
+    public static void deleteStudent(ArrayList<Student> students) {
         int lengthOfStudents = students.size();
         int i = 0;
-        while (i<lengthOfStudents){
-            if (averageGrade(students.get(i).grades)<3){
+        while (i < lengthOfStudents) {
+            if (averageGrade(students.get(i).grades) < 3) {
                 students.remove(i);
                 lengthOfStudents--;
                 students.get(i).cours++;
-            }
-            else i++;
+            } else i++;
         }
-}
-public static void
+    }
+
+    public static void
 }
 
