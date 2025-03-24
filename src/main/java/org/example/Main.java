cde6a540ec3677f269bcdde86edc7dd266169190
galinaB-95 <galinabilokur@gmail.com>
2025-03-24 19:38:54 +0300

    l

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,21 @@
 package org.example;
 
+import java.math.BigInteger;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
+        public static BigInteger getFactorial (int f) {
+            BigInteger result = BigInteger.ONE;
+            for (int i = 1; i<=f; i++)
+                result = result.multiply(BigInteger.valueOf(i));
+            return result;
         }
-    }
+
+        public double area(double h, double a) {
+            return (h * a) / 2;
+        }
+        public static void
+        }
+
