829b8e6d495868e60e9acc4c9369cc651d6e69f3
galinaB-95 <galinabilokur@gmail.com>
2025-03-24 20:32:16 +0300

    l

--- src/main/java/org/example/ClassForTests.java
+++ src/main/java/org/example/ClassForTests.java
@@ -4,7 +4,7 @@ import java.math.BigInteger;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class ClassForTests {
 
     public static BigInteger getFactorial(int f) {
         BigInteger result = BigInteger.ONE;
