4660579f7acc469dfb7dac72ec70ee99a4ab852b
galinaB-95 <galinabilokur@gmail.com>
2025-03-24 19:58:51 +0300

    l

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,16 +6,45 @@ import java.math.BigInteger;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-        public static BigInteger getFactorial (int f) {
-            BigInteger result = BigInteger.ONE;
-            for (int i = 1; i<=f; i++)
-                result = result.multiply(BigInteger.valueOf(i));
-            return result;
-        }
+    public static BigInteger getFactorial(int f) {
+        BigInteger result = BigInteger.ONE;
+        for (int i = 1; i <= f; i++)
+            result = result.multiply(BigInteger.valueOf(i));
+        return result;
+    }
 
-        public double area(double h, double a) {
-            return (h * a) / 2;
+    public double area(double h, double a) {
+        return (h * a) / 2;
+    }
+
+    public static int add(int a, int b) {
+
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+
+        return a - b;
+    }
+
+    public static int multipy(int a, int b) {
+
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+
+        return a / b;
+    }
+
+    public static void comparing(int a, int b) {
+        if (a > b) {
+            System.out.println(a > b);
+        } else if (a < b) {
+            System.out.println(a < b);
+        } else {
+            System.out.println(a = b);
         }
-        public static void
-        }
+    }
+}
 
