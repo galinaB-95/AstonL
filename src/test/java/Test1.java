18d40b36769a4991dbd6f3c1697e2ecdf0f99226
galinaB-95 <galinabilokur@gmail.com>
2025-04-01 22:11:36 +0300

    l

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test1 {
@@ -13,6 +14,20 @@ public class Test1 {
                 driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/h2")).getText(),"Онлайн пополнение\n" +
                 "без комиссии" );
         driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/h2")).getText();
+    }
+    @Test
+    public void logo(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.mts.by/");
+
+        WebElement element = driver.findElement(By.xpath("//ul[1]"));
+
+        System.out.println(element.getText());
+
+
+//*[@id="pay-section"]/div/div/div[2]/section/div/div[2]/ul/li[1]/img
+        //*[@id="pay-section"]/div/div/div[2]/section/div/div[2]/ul
+
 
     }
 }
