950d86829b5c1685b070373bd4fd3b069f406381
galinaB-95 <galinabilokur@gmail.com>
2025-04-03 12:06:14 +0300

    l

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -42,12 +42,18 @@ public class Test1 {
     public void keep() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.mts.by/");
-        //new WebDriverWait(driver, Duration.ofSeconds(10)).
-        //until(ExpectedConditions.elementToBeClickable(By.id("cookie-agree"))).click();
-        driver.manage().window().maximize();
-        driver.findElement(By.xpath("//from[@id='pay-connection']/div[1]")).sendKeys("297777777");
-    }
 
+        new WebDriverWait(driver, Duration.ofSeconds(20)).
+                until(ExpectedConditions.elementToBeClickable(By.id("cookie-agree"))).click();
+        driver.manage().window().maximize();
+        driver.findElement(By.id("connection-phone")).sendKeys("297777777");
+        driver.findElement(By.id("connection-sum")).sendKeys("200");
+        driver.findElement(By.id("connection-email")).sendKeys("2334@fc.com");
+        driver.findElement(By.xpath("//button[text()='Продолжить']")).click();
+    }
+//*[@id="connection-phone"]//*[@id="connection-email"]//*[@id="pay-connection"]/button
+
+    //*[@id="connection-sum"]
 
 }//*[@id="cookie-agree"]
 // driver.findElement(By.id("cookie-agree")).click();
