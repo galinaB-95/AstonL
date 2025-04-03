c27cf289d66ae8b4a6c7cdc3a980cd7d55e9a3ff
galinaB-95 <galinabilokur@gmail.com>
2025-04-03 22:25:35 +0300

    l

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
@@ -6,8 +7,15 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Test1 {
+
+
     @Test
     public void init() {
         WebDriver driver = new ChromeDriver();
@@ -24,11 +32,17 @@ public class Test1 {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.mts.by/");
 
-        driver.findElement(By.xpath("//ul[1]"));
+        driver.findElement(By.cssSelector("img[src*=visa]"));
+        driver.findElement(By.cssSelector("img[src*=visa-verified]"));
+        driver.findElement(By.cssSelector("img[src*=mastercard]"));
+        driver.findElement(By.cssSelector("img[src*=mastercard-secure]"));
+        driver.findElement(By.cssSelector("img[src*=belkart]"));
+        System.out.println("Логотипы найдены");
+        driver.quit();
     }
 
     @Test
-    public void link()  {
+    public void link() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.mts.by/");
         new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.elementToBeClickable(By.id("cookie-agree"))).click();
@@ -42,7 +56,6 @@ public class Test1 {
     public void keep() {
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.mts.by/");
-
         new WebDriverWait(driver, Duration.ofSeconds(20)).
                 until(ExpectedConditions.elementToBeClickable(By.id("cookie-agree"))).click();
         driver.manage().window().maximize();
@@ -52,7 +65,11 @@ public class Test1 {
         driver.findElement(By.xpath("//button[text()='Продолжить']")).click();
         driver.quit();
     }
+
 }
+
+
+
 
 
 
