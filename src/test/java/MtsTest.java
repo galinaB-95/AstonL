0805301adbff1ed7c8e96b0d4289fe8787456012
galinaB-95 <galinabilokur@gmail.com>
2025-04-11 14:22:18 +0300

    l

---
+++ src/test/java/MtsTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class MtsTest {
+    @Test
+    public void Communication(){
+        CommunicationSerivices CommunicationSerivices = new CommunicationSerivices();
+    }
+}
