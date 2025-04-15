6ecd269b6b9efc690b7cc239d3bef4c8a2f53cbe
galinaB-95 <galinabilokur@gmail.com>
2025-04-15 11:46:03 +0300

    l

--- src/test/java/Postman.java
+++ src/test/java/Postman.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -7,28 +8,77 @@ import static org.hamcrest.Matchers.equalTo;
 
 
 public class Postman {
+
+
     @Test
-    public void get(){
-     given()
-               .baseUri("https://postman-echo.com")
-                .params("foo1", "bar1")
-                .params("foo2","bar2")
-               .when()
-               .get("/get")
-             .then()
-             .body("args.foo1",equalTo("bar1"))
-             .body("args.foo2",equalTo("bar2"));
-    }
-    @Test
-    public void post(){
-        HashMap<String,String> body = new HashMap<>();
-        body.put("data","This is expected to be sent back as part of response body");
+    public void get() {
         given()
                 .baseUri("https://postman-echo.com")
+                .params("foo1", "bar1")
+                .params("foo2", "bar2")
                 .when()
-                .body(body)
+                .get("/get")
+                .then()
+                .body("args.foo1", equalTo("bar1"))
+                .body("args.foo2", equalTo("bar2"));
+    }
+
+    @Test
+    public void post() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("This is expected to be sent back as part of response body")
+                .when()
                 .post("/post")
                 .then()
-                .body("post",equalTo("https://postman-echo.com/post"));
+                .body("data", equalTo("This is expected to be sent back as part of response body"));
+
+    }
+
+    @Test
+    public void postData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .formParam("foo1", "bar1")
+                .formParam("foo2", "bar2")
+                .header("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8")
+                .when()
+                .post("/post")
+                .then()
+                .body("form.foo1", equalTo("bar1"))
+                .body("form.foo2", equalTo("bar2"));
+    }
+
+    @Test
+    public void put() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("This is expected to be sent back as part of response body")
+                .when()
+                .put("/put")
+                .then()
+                .body("data", equalTo("This is expected to be sent back as part of response body"));
+    }
+
+    @Test
+    public void patch() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("This is expected to be sent back as part of response body")
+                .when()
+                .patch("/patch")
+                .then()
+                .body("data", equalTo("This is expected to be sent back as part of response body"));
+    }
+
+    @Test
+    public void delete() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("This is expected to be sent back as part of response body")
+                .when()
+                .delete("/delete")
+                .then()
+                .body("data", equalTo("This is expected to be sent back as part of response body"));
     }
 }
